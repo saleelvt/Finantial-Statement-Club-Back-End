49093b36ad4f2721d1b7415088bb6247c0b8972a
saleel vt <saleelvt57@gmail.com>
2025-03-07 02:35:21 +0530

    the data

--- src/infrastructure/routers/adminRouter.ts
+++ src/infrastructure/routers/adminRouter.ts
@@ -1,7 +1,6 @@
 import { adminController } from '@/presentation/admin/controllers';
 import { IAdminDependencies } from './../../application/admin/interfaces/IAdminDependencies';
 import { Router } from 'express';
-import { authMiddleware } from '@/utilities/Auth/authMiddleware';
 import upload from '@/utilities/multer/multer';
 
 export const adminRoutes = (dependencies: IAdminDependencies) => {
@@ -9,19 +8,19 @@ export const adminRoutes = (dependencies: IAdminDependencies) => {
     const router = Router();
     router.route("/login").post(loginAdmin);
     router.route("/verifyOtp").post(verifyOtp);
-    router.route("/logout").delete(authMiddleware,logoutAdmin);
-    router.route("/addDocumentEnglish").post(authMiddleware,upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),addDocument);
-    router.route("/addDocumentArabic").post(authMiddleware,upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),addDocumentArabic);
-    router.route("/addTable").post(authMiddleware,upload.fields([{name:"screenshotFile"}]),addTable);
-    router.route("/deleteDocument/:docToDelete").delete(authMiddleware,deleteDocument);
+    router.route("/logout").delete(logoutAdmin);
+    router.route("/addDocumentEnglish").post(upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),addDocument);
+    router.route("/addDocumentArabic").post(upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),addDocumentArabic);
+    router.route("/addTable").post(upload.fields([{name:"screenshotFile"}]),addTable);
+    router.route("/deleteDocument/:docToDelete").delete(deleteDocument);
     router.route("/getDocuments").get(getAllDocuments);
     router.route("/getArabicDocuments").get(getAllArabicDocuments);
     router.route("/getDocumetnBytadawalCode").get(getDocumetnByNickName);
     router.route('/tadawalCodeSuggestions').get(getNicknamesSuggestions)
     router.route('/getDataWithSuggestions').get(getDataWithSuggestions)
     router.route('/getDataWithSuggestionsForTable').get(getDataWithSuggestionsForTable)
-    router.route('/getDocumentById/:id').get(authMiddleware,getDocumentById)
-    router.route('/updateDocumentEnglish').put(authMiddleware,upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),updateDocumentEnglish)
-    router.route('/updateDocumentArabic').put(authMiddleware,upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),updateDocumentArabic)
+    router.route('/getDocumentById/:id').get(getDocumentById)
+    router.route('/updateDocumentEnglish').put(upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),updateDocumentEnglish)
+    router.route('/updateDocumentArabic').put(upload.fields([{ name: "Board" }, { name: "Q1" }, { name: "Q2" }, { name: "Q3" }, { name: "Q4" }, { name: "S1" }, { name: "Year" }]),updateDocumentArabic)
     return router;
 };
